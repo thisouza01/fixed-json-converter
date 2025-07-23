ce7781355dbbb04a2bac48b1c0ec52f2794a5e54
Thiago <tihsouza01@gmail.com>
2025-07-23 10:17:08 -0300

    Add: Criação da classe usuário e leitura do arquivo JSON.

---
+++ src/UserData.java
@@ -0,0 +1,29 @@
+public class UserData {
+    private String name;
+    private String email;
+    private double balance;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
